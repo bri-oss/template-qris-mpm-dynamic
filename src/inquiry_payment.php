6b863e9d6de5cca5e47a12c001303fccc5fae07a
AlfaRiza <malfariza45@gmail.com>
2025-01-21 14:46:03 +0700

    feat: xss attact and declare try catch

--- src/inquiry_payment.php
+++ src/inquiry_payment.php
@@ -3,6 +3,9 @@ require 'utils.php';
 
 use BRI\Util\VarNumber;
 
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+header('Content-Type: application/json'); // Ensure all responses are JSON
+
 // url path values
 $baseUrl = 'https://sandbox.partner.api.bri.co.id'; //base url
 
@@ -47,9 +50,23 @@ try {
     $body
   );
 
-  echo $response;
+  echo htmlspecialchars($response, ENT_QUOTES, 'UTF-8');
+} catch (InvalidArgumentException $e) {
+  // Return a generic error message to the client
+  http_response_code(400); // Bad Request
 
+  // Log the error for debugging
+  error_log('InvalidArgumentException: ' . $e->getMessage());
+} catch (RuntimeException $e) {
+  // Return a generic error message to the client
+  http_response_code(500); // Internal Server Error
+
+  // Log the error for debugging
+  error_log('RuntimeException: ' . $e->getMessage());
 } catch (Exception $e) {
-  error_log('Error: ' . $e->getMessage());
-  exit(1);
+  // Return a generic error message to the client
+  http_response_code(500); // Internal Server Error
+
+  // Log any other unexpected errors for debugging
+  error_log('UnexpectedException: ' . $e->getMessage());
 }
